8d857194b559bf693a4aa6c720e3ac50221e02ea
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2024-06-25 14:07:32 +0900

    Initial Commit

---
+++ MovieLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieLog
+//
+//  Created by 최승범 on 6/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
